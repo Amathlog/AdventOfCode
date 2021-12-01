a80e551d487efd224e85dade1ca6ea0feaa31a1d
alogut <adrien.logut@ubisoft.com>
2021-12-01 18:05:52 -0500

    Fix script + day 8

--- create_new_folder.py
+++ create_new_folder.py
@@ -15,7 +15,7 @@ with entry_file.open("r") as f:
     entries = f.readlines()
 
 for i in range(len(entries)):
-    if entries[i][-1] == '\n':
+    if entries[i][-1] == '\\n':
         entries[i] = entries[i][:-1]
 """
 
