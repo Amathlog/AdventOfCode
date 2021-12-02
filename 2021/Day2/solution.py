8151ba223f753c0db2458a7c63b2299663298ed9
alogut <adrien.logut@ubisoft.com>
2021-12-02 00:09:46 -0500

    Day 2 2021

--- 2021/Day2/solution.py
+++ 2021/Day2/solution.py
@@ -9,3 +9,38 @@ with entry_file.open("r") as f:
 for i in range(len(entries)):
     if entries[i][-1] == '\n':
         entries[i] = entries[i][:-1]
+
+
+x = 0
+y = 0
+
+for e in entries:
+    dir, i = e.split(" ")
+    i = int(i)
+
+    if dir == "forward":
+        x += i
+    elif dir == "down":
+        y += i
+    else:
+        y -= i
+
+print("First anwser:", x * y)
+
+x = 0
+y = 0
+aim = 0
+
+for e in entries:
+    dir, i = e.split(" ")
+    i = int(i)
+
+    if dir == "forward":
+        x += i
+        y += i * aim
+    elif dir == "down":
+        aim += i
+    else:
+        aim -= i
+
+print("Second anwser:", x * y)
