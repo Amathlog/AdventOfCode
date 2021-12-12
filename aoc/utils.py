6db7c2828721d8b685d11263e097e4f9c78cd12f
alogut <adrien.logut@ubisoft.com>
2021-12-12 14:36:58 -0500

    Fix utils

--- aoc/utils.py
+++ aoc/utils.py
@@ -1,7 +1,7 @@
 import itertools
 
 def neighboor_iter(x: int, y: int, limit_x: int, limit_y: int, discard_center: bool = True, discard_diagonal: bool = False):
-    inner_range = ((1, 0), (-1, 0), (0, 1), (0, -1)) if discard_diagonal else itertools.product((-1, 0, 1), (-1, 0, 1))
+    inner_range = ((0, 0), (1, 0), (-1, 0), (0, 1), (0, -1)) if discard_diagonal else itertools.product((-1, 0, 1), (-1, 0, 1))
     for incr_x, incr_y in inner_range:
         if incr_x == 0 and incr_y == 0 and discard_center:
             continue
