53472467b37b6b5a8bed9ec6b1aec5061c5c86a5
Adrien Logut <newsadrien@gmail.com>
2022-12-13 13:56:25 -0500

    Cleanup 2022 Day13

--- aoc/2022/Day13/solution.py
+++ aoc/2022/Day13/solution.py
@@ -13,24 +13,6 @@ for i in range(len(entries)):
     if entries[i][-1] == '\n':
         entries[i] = entries[i][:-1]
 
-def extract_list(input: str) -> str:
-    if input == "" or input[0] != "[":
-        return ""
-
-    i = 1
-    sub_lists = 0
-    while i < len(input):
-        if input[i] == "[":
-            sub_lists += 1
-        elif input[i] == "]":
-            sub_lists -= 1
-            if sub_lists == -1:
-                return input[:i+1]
-        
-        i += 1
-
-    return ""
-
 
 class Node:
     def __init__(self, value: Optional[int] = None):
@@ -46,29 +28,17 @@ class Node:
 
     @staticmethod
     def from_input(input: str) -> "Node":
-        if input == "":
-            return Node()
+        return Node.from_list_int(eval(input))
 
-        if input[0] != "[":
-            return Node(int(input))
-
+    @staticmethod
+    def from_list_int(list_in: List) -> "Node":
         res = Node()
-        i = 1
-        while i < len(input) - 1:
-            if input[i] == "[":
-                sub_list = extract_list(input[i:])
-                assert sub_list != ""
-
-                res.add_child(Node.from_input(sub_list))
-                i += len(sub_list)
-            elif input[i] == ",":
-                i += 1
+        for item in list_in:
+            if type(item) is int:
+                res.add_child(Node(item))
             else:
-                value = input[i:].split(",")[0]
-                int_value = int(value) if value[-1] != "]" else int(value[:-1])
-                res.add_child(Node(int_value))
-                i += len(value)
-
+                res.add_child(Node.from_list_int(item))
+        
         return res
 
     def __repr__(self) -> str:
@@ -101,49 +71,41 @@ class Node:
     def __lt__(self, other: "Node") -> bool:
         return self.compare(other) < 0
 
+
 if __name__ =="__main__":
-    pairs: List[Tuple[Node, Node]] = []
     all_packets: List[Node] = []
 
-    part1_res = 0
+    sum_of_valid_pairs = 0
 
     for i in range(len(entries) // 3 + 1):
         entry1 = entries[3*i]
         entry2 = entries[3*i + 1]
 
-        pairs.append((Node.from_input(entry1), Node.from_input(entry2)))
-        all_packets.extend(pairs[-1])
+        node1 = Node.from_input(entry1)
+        node2 = Node.from_input(entry2)
 
-        comparison = pairs[-1][0].compare(pairs[-1][1])
-        if comparison < 0:
-            # Right order
-            part1_res += i + 1
+        all_packets.extend((node1, node2))
 
-    print("Part 1: Sum of valid pairs =", part1_res)
+        # Right order
+        if node1 < node2:
+            sum_of_valid_pairs += i + 1
+
+    print("Part 1: Sum of valid pairs =", sum_of_valid_pairs)
 
     # Adding 2 packets
-    divider_packet1 = Node()
-    temp = Node()
-    temp.add_child(Node(2))
-    divider_packet1.add_child(temp)
-    all_packets.append(divider_packet1)
+    divider_packet1 = Node.from_list_int([[2]])
+    divider_packet2 = Node.from_list_int([[6]])
 
-    divider_packet2 = Node()
-    temp2 = Node()
-    temp2.add_child(Node(6))
-    divider_packet2.add_child(temp2)
-    all_packets.append(divider_packet2)
-
+    all_packets.extend((divider_packet1, divider_packet2))
     all_packets.sort()
 
-    part2_res = 1
+    decoder_key = 0
 
     for i in range(len(all_packets)):
         if all_packets[i] == divider_packet1:
-            part2_res *= i+1
-        
+            decoder_key = i+1
         elif all_packets[i] == divider_packet2:
-            part2_res *= i+1
+            decoder_key *= i+1
             break
 
-    print("Part2: Decoder key =", part2_res)
+    print("Part2: Decoder key =", decoder_key)
