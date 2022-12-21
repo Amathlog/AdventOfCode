188b0be0647491d24a5ab4177a8a455cb8302170
Adrien Logut <newsadrien@gmail.com>
2022-12-21 14:18:49 -0500

    Add another solution

--- aoc/2022/Day19/solution.py
+++ aoc/2022/Day19/solution.py
@@ -2,6 +2,9 @@ from pathlib import Path
 import os
 from typing import List, Tuple, Any
 import re
+import sys
+import math
+import time
 
 entry_file = Path(os.path.abspath(__file__)).parent / "entry.txt"
 example_file = Path(os.path.abspath(__file__)).parent / "example.txt"
@@ -20,6 +23,17 @@ entries = parse_entry(entry_file)
 example_entries = parse_entry(example_file)
 
 
+class Profiling:
+    def __init__(self):
+        self.start = 0
+
+    def __enter__(self):
+        self.start = time.perf_counter()
+
+    def __exit__(self, *args):
+        print(f"Time taken = {(time.perf_counter() - self.start) * 1000:.2f}ms")
+
+
 class Resources:
     def __init__(self, ore: int = 0, clay: int = 0, obsidian: int = 0, geode: int = 0):
         self.ore = ore
@@ -32,8 +46,10 @@ class Resources:
 
     def __sub__(self, other: "Resources") -> "Resources":
         res = Resources(self.ore - other.ore, self.clay - other.clay, self.obsidian - other.obsidian, self.geode - other.geode)
-        assert res.ore >= 0 and res.clay >= 0 and res.obsidian >= 0 and res.geode >= 0
         return res
+
+    def __mul__(self, other: int) -> "Resources":
+        return Resources(self.ore * other, self.clay * other, self.obsidian * other, self.geode * other)
 
     def __iadd__(self, other: "Resources") -> "Resources":
         self.ore += other.ore
@@ -48,8 +64,6 @@ class Resources:
         self.clay -= other.clay
         self.obsidian -= other.obsidian
         self.geode -= other.geode
-
-        assert self.ore >= 0 and self.clay >= 0 and self.obsidian >= 0 and self.geode >= 0
 
         return self
 
@@ -116,6 +130,43 @@ class Blueprint:
     # We cannot have enough of geode! We try to maximize it!
     def can_construct_geode(self, robot: "Resources", res: "Resources", minutes: int) -> bool:
         return res.ore >= self.geode_robot_cost.ore and res.obsidian >= self.geode_robot_cost.obsidian
+
+    # Compute the number of minutes to construct this robot
+    def time_to_construct_ore(self, robot: "Resources", res: "Resources", wanted_cost: Resources = None):
+        ore_cost = (self.ore_robot_cost.ore - res.ore) if wanted_cost is None else (wanted_cost.ore - res.ore)
+        if ore_cost <= 0:
+            return 0
+        
+        return math.ceil(ore_cost / robot.ore)
+
+    def time_to_construct_clay(self, robot: "Resources", res: "Resources"):
+        return self.time_to_construct_ore(robot, res, self.clay_robot_cost)
+
+    def time_to_construct_obsidian(self, robot: Resources, res: Resources):
+        if robot.clay == 0:
+            return sys.maxsize
+
+        ore_time = self.time_to_construct_ore(robot, res, self.obsidian_robot_cost)
+        clay_cost = self.obsidian_robot_cost.clay - res.clay
+        if clay_cost <= 0:
+            clay_time = 0
+        else:
+            clay_time = math.ceil(clay_cost / robot.clay)
+
+        return max(ore_time, clay_time)
+
+    def time_to_construct_geode(self, robot: Resources, res: Resources):
+        if robot.obsidian == 0:
+            return sys.maxsize
+
+        ore_time = self.time_to_construct_ore(robot, res, self.geode_robot_cost)
+        obsidian_cost = self.geode_robot_cost.obsidian - res.obsidian
+        if obsidian_cost <= 0:
+            obsidian_time = 0
+        else:
+            obsidian_time = math.ceil(obsidian_cost / robot.obsidian)
+
+        return max(ore_time, obsidian_time)
 
     @staticmethod
     def from_input(input: str) -> "Blueprint":
@@ -203,30 +254,85 @@ def simulation_impl(blueprint: Blueprint, minutes: int, resources: Resources = N
     return res, count, best_robots
 
 
-def solve(entries: List[str]):
+# Thanks Ju Marlow
+def other_simulation(blueprint: Blueprint, minutes: int) -> Tuple[int, int, Any]:
+    blueprint.set_max_minutes(minutes)
+    return other_simulation_impl(blueprint, minutes, Resources(), Resources(ore=1))
+
+def other_simulation_impl(blueprint: Blueprint, minutes: int, resources: Resources, robots: Resources) -> Tuple[int, int, Any]:
+    best_estimated = robots.geode * minutes
+    if minutes == 1:
+        return best_estimated, 1, robots
+
+    new_state = (
+        (False, blueprint.time_to_construct_geode, blueprint.geode_robot_cost),
+        (blueprint.has_enough_obsidian(robots, resources, minutes), blueprint.time_to_construct_obsidian, blueprint.obsidian_robot_cost),
+        (blueprint.has_enough_clay(robots, resources, minutes), blueprint.time_to_construct_clay, blueprint.clay_robot_cost),
+        (blueprint.has_enough_ore(robots, resources, minutes), blueprint.time_to_construct_ore, blueprint.ore_robot_cost)
+    )
+
+    count = 0
+    best_robots = robots
+
+    for (has_enough, time_to_construct_func, cost), new_robot in zip(new_state, new_robots):
+        if has_enough:
+            continue
+
+        time_to_construct = time_to_construct_func(robots, resources) + 1
+        if time_to_construct > minutes:
+            continue
+
+        new_resources = resources + (robots * time_to_construct) - cost
+
+        result, incr, final_robots = other_simulation_impl(blueprint, minutes - time_to_construct, new_resources, robots + new_robot)
+        result += robots.geode * time_to_construct
+        count += incr
+        if result > best_estimated:
+            best_estimated = result
+            best_robots = final_robots
+
+    return best_estimated, count, best_robots
+
+
+def solve(entries: List[str], try_other_simulation: bool):
     blueprints = [Blueprint.from_input(e) for e in entries]
 
     res = 0
 
     for bp in blueprints:
-        nb_geodes, branches, robots = simulation(bp, 24)
+        if try_other_simulation:
+            nb_geodes, branches, robots = other_simulation(bp, 24)
+        else:
+            nb_geodes, branches, robots = simulation(bp, 24)
+
         print(f"Blueprint {bp.id} has gather {nb_geodes} for a quality level of {bp.id * nb_geodes} with {branches} branches. Final robots: {robots}")
         res += bp.id * nb_geodes
 
     print("Part 1: Res =", res)
 
-def solve2(entries: List[str]):
+def solve2(entries: List[str], try_other_simulation: bool):
     blueprints = [Blueprint.from_input(e) for e in entries[:3]]
 
     res = 1
 
     for bp in blueprints:
-        nb_geodes, branches, robots = simulation(bp, 32)
+        if try_other_simulation:
+            nb_geodes, branches, robots = other_simulation(bp, 32)
+        else:
+            nb_geodes, branches, robots = simulation(bp, 32)
+
         print(f"Blueprint {bp.id} has gather {nb_geodes} with {branches} branches. Final robots: {robots}")
         res *= nb_geodes
 
     print("Part 2: Res =", res)
 
 if __name__ == "__main__":
-    solve(entries)
+    print("My solution:")
+    with Profiling():
+        solve(entries, False)
+
+    print("Other solution:")
+    with Profiling():
+        solve(entries, True)
+
     solve2(entries)
